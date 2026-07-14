be83d3461d852c15b22a442d06956cad1bd0fd7a
abinaya <abinayakumar869@gmail.com>
2026-07-14 22:45:57 +0530

    added products page

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,11 +1,101 @@
+import { useEffect, useState } from "react";
 import DashboardLayout from "../layouts/DashboardLayout";
+import { orderAPI } from "../services/api";
 
 function Orders() {
-  return (
-    <DashboardLayout>
-      <h1>Orders</h1>
-    </DashboardLayout>
-  );
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        loadOrders();
+    }, []);
+
+    const loadOrders = async () => {
+
+        try {
+
+            const res = await orderAPI.getAll();
+
+            setOrders(res.data);
+
+        } catch (error) {
+
+            console.log(error);
+
+        }
+
+    };
+
+    return (
+
+        <DashboardLayout>
+
+            <h1>Orders</h1>
+
+            <table className="product-table">
+
+                <thead>
+
+                    <tr>
+
+                        <th>Customer</th>
+
+                        <th>Product</th>
+
+                        <th>Quantity</th>
+
+                        <th>Status</th>
+
+                        <th>Delivery Date</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {
+
+                        orders.map(order => (
+
+                            <tr key={order._id}>
+
+                                <td>{order.customerName}</td>
+
+                                <td>
+                                    {
+                                        typeof order.product === "object"
+                                        ? order.product.name
+                                        : order.product
+                                    }
+                                </td>
+
+                                <td>{order.quantity}</td>
+
+                                <td>{order.status}</td>
+
+                                <td>
+                                    {
+                                        new Date(
+                                            order.deliveryDate
+                                        ).toLocaleDateString()
+                                    }
+                                </td>
+
+                            </tr>
+
+                        ))
+
+                    }
+
+                </tbody>
+
+            </table>
+
+        </DashboardLayout>
+
+    );
+
 }
 
 export default Orders;
